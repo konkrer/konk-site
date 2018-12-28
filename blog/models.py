84679bd2ed338d062d380a70420c03e089a76525
konkrer <rivortex@gmail.com>
2018-12-28 05:00:12 -0800

    more

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,6 @@ from django.db import models
 class Blog(models.Model):
 	image = models.ImageField(upload_to='images/')
 	title = models.CharField(max_length=100)
-	pub_date = models.DateField(auto_now=True)
+	pub_date = models.DateField()
 	body = models.TextField()
 
