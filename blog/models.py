e83ff1b5bf647fd571caf81e3f97bb488d7b1d45
konkrer <rivortex@gmail.com>
2018-12-27 16:56:50 -0800

    blog

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Blog(models.Model):
+	image = models.ImageField(upload_to='images/')
+	title = models.CharField(max_length=100)
+	#pub_date = models.DateField(default='1975-04-03')
+	body = models.TextField()
+
